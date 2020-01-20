d9d47c556a9a2aa9920da7fc92aaf4a520aadacc
RitikaJain8818 <46858658+RitikaJain8818@users.noreply.github.com>
2020-01-20 14:06:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express= require('express');
+const expressLayouts=require('express-ejs-layouts');
+const mongoose=require('mongoose');
+const flash=require('connect-flash');
+const session=require('express-session');
+const passport=require('passport');
+const app=express();
+
+//Passsport config
+require('./config/passport')(passport);
+
+//DB config
+const db=require('./config/keys').MongoURI;
+
+//Connect to Mongo
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(db, { useNewUrlParser: true ,useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("test").collection("users");
+ // perform actions on the collection object
+  client.close();
+});
+//mongoose.connect(db, {useNewUrlParser: true})
+    //.then(() => console.log('MongoDB Connected....'))
+   // .catch(err=>console.log(err));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: false}));
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Passport MiddleWare
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+//Global Variables
+app.use((req, res, next)=>{
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
+    res.locals.error=req.flash('error'); 
+    next();
+});
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+const PORT=process.env.PORT || 8000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MongoURI: 'mongodb+srv://ritikajain:bootstrap@cluster0-uvuct.mongodb.net/test?retryWrites=true'
+}
